dfb1cd7302a5af3920de7d574571dd3ba200935d
Vinicius-Muxila <viniciuss.nuness@gmail.com>
2023-08-10 22:25:53 -0300

    Primeiro Commit

---
+++ DesignPatterns/src/br/com/fiap/patterns/model/Pis.java
@@ -0,0 +1,21 @@
+package br.com.fiap.patterns.model;
+
+import java.util.Observable;
+
+public class Pis extends Observable implements Imposto{
+	
+	private float aliquota = 0.6f;
+	
+	private float valorPis;
+
+	@Override
+	public void calcularImposto(float valor) {
+		setChanged();
+		valorPis = valor * aliquota;
+		notifyObservers(valorPis);
+	}
+
+	public float getValorPis() {
+		return valorPis; 
+	}
+}
